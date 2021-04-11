f3d3c5dac3301548c5182399eec57e17d36e757b
AmritSap <amritsapkota32@gmail.com>
2021-04-11 12:43:11 +1000

    layout updaed

--- src/components/add-category-form/AddCategoryForm.js
+++ src/components/add-category-form/AddCategoryForm.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Button, Form } from "react-bootstrap";
 
+const initialState = {
+  name: "",
+  parentCategory: "0",
+};
 export const AddCategoryForm = () => {
+  const [category, setCategory] = useState(initialState);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setCategory({
+      ...category,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = (e) =>{ e.preventDefault();
+      console.log(category)
+
+
+}
+  
+  // we going to find the way to call sever
+
   return (
     <div className="add-category-form">
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <Form.Row>
           <Form.Group as={Col} controlId="">
             <Form.Label>New Category</Form.Label>
             <Form.Control
-              name="category"
+              value={category.name}
+              name="name"
               type="text"
+              onChange={handleOnChange}
               placeholder="Enter new category"
+              required
             />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridState">
             <Form.Label>Select Parent Category</Form.Label>
-            <Form.Control as="select" defaultValue="0">
+            <Form.Control
+              as="select"
+              name="parentCategory"
+              onChange={handleOnChange}
+              defaultValue={category.parentCategory}
+            >
               <option>Choose...</option>
               <option>...</option>
             </Form.Control>
--- src/components/layout/partials/Header.js
+++ src/components/layout/partials/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
-          <i class="fas fa-bell text-success"></i>
+          <i className="fas fa-bell text-success"></i>
         </Navbar.Text>
         <Navbar.Text>
-          <i class="fas fa-user text-primary"></i> Log Out
+          <i className="fas fa-user text-primary"></i> Log Out
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
